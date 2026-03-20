cd03e08c8717c369c3d35515a9fb8ae3a1ae503c
Rukman <rukmanbcs@gmail.com>
2026-03-20 14:17:07 +0530

    fix(DatabaseSeeder.php): Change seeder to populate only stakeholders and spatie data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,11 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SpatieSeeder::class,
             StakeholderSeeder::class,
-            ProgrammeDetailsSeeder::class,
         ]);
         return;
     }
@@ -31,8 +26,6 @@ class DatabaseSeeder extends Seeder
         $options = [
             'spatie' => 'SpatieSeeder (Roles & Permissions)',
             'stakeholder' => 'StakeholderSeeder (Users & Students)',
-            'programme' => 'ProgrammeDetailsSeeder (Programmes, Levels, Skills, etc.)',
-            'studentPortal' => 'StudentPortalSeeder(StudentOptionalModuleSeeder, etc.)',
             'all' => 'Run ALL seeders',
             'none' => 'Exit without seeding',
         ];
@@ -46,13 +39,9 @@ class DatabaseSeeder extends Seeder
         match ($choice) {
             'spatie' => $this->call([SpatieSeeder::class]),
             'stakeholder' => $this->call([StakeholderSeeder::class]),
-            'programme' => $this->call([ProgrammeDetailsSeeder::class]),
-            'studentPortal' => $this->call([StudentPortalSeeder::class]),
             'all' => $this->call([
                 SpatieSeeder::class,
                 StakeholderSeeder::class,
-                ProgrammeDetailsSeeder::class,
-                StudentPortalSeeder::class,
             ]),
             default => $this->command->info('No seeders were run.'),
         };
